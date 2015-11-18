a31d996a9099ff6335bd048d8551b1951f8577a8
Nicolas Kayser-Bril <hi@nkb.fr>
2015-11-18 12:25:07 +0100

    changed model to intercept=0

--- analyses/price_per_sqm.py
+++ analyses/price_per_sqm.py
@@ -4,14 +4,14 @@
 import numpy as np
 import csv as csv
 import pandas as pd
-import scipy.stats as sc
+import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 import pylab
 from PIL import Image
 
-readdata = csv.reader(open("../data/20151113/ads.csv"))
+readdata = csv.reader(open("data/20151113/ads.csv"))
 
-im = Image.open('../images/logo.png')
+im = Image.open('images/logo.png')
 height = im.size[1]
 
 im = np.array(im).astype(np.float) / 255
@@ -58,6 +58,10 @@ figure = plt.figure(1, figsize=(8, 12), dpi=80)
 
 city_counter = 0
 
+# Prepares the CSV with the data
+
+csv = "city, price_per_sqm_reg, price_per_sqm_avg, slope\n"
+
 for city in cities:
 
 	city_counter += 1
@@ -75,17 +79,32 @@ for city in cities:
 	plt.xlim([0,2000])
 	ax.plot(table_city['total_rent'], table_city['living_space'], 'bo', alpha=.2, ms=1)
 
-	regressionline = sc.stats.linregress(table_city['total_rent'], table_city['living_space'])
+	model = smf.ols(formula='living_space ~ total_rent - 1', data=table_city)
+	results = model.fit()
 
-	m = regressionline[0]
-	b = regressionline[1]
-	x = np.linspace(0, 3000, 100)
+	# Prints the summary to the console
+	print "\n" + city + "\n=============\n"
+	print results.summary()
 
-	price_per_sqm = round(1/m, 1)
+	m = results.params.total_rent
 
-	ax.set_title(city.capitalize() + ": " + str(price_per_sqm) + "EUR/sqm (n=" +  str(len(table_city.index)) + ")")
+	x = np.linspace(200, 3000, 100)
 
-	ax.plot(x, m*x + b)
+	# Computes price per sqm
+	price_per_sqm = str(round(1/m, 1))
+
+	# Appends data to CSV
+	csv += city + ','
+	csv += price_per_sqm + ',' 
+	csv += str(table_city['price_per_sqm'].mean()) + ','
+	csv += str(m)
+	csv +='\n'
+
+	# Writes the title of each subgraph
+	ax.set_title(city.capitalize() + ": " + price_per_sqm + "EUR/sqm (n=" +  str(len(table_city.index)) + ")")
+
+	# Plots the regression line
+	ax.plot(x, m*x)
 
 # Makes layout more legible
 figure.tight_layout()
@@ -95,4 +114,9 @@ img = figure.figimage(im, 0, figure.bbox.ymax - height, alpha=.05)
 img.set_zorder(20)
 
 # Saves image
-pylab.savefig('price_vs_area.png')
+pylab.savefig('analyses/price_vs_area.png')
+
+# Saves CSV
+f = open("analyses/price_per_sqm.csv", "w")
+f.write(csv)
+f.close()
